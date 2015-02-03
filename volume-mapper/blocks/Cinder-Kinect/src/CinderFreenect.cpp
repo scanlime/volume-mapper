6082b719dfbaa7d0863b5f138d393d0d2d4e1001
Micah Elizabeth Scott <micah@misc.name>
2015-02-02 16:37:28 -0800

    CinderFreenect patch, disable depth transform

--- volume-mapper/blocks/Cinder-Kinect/src/CinderFreenect.cpp
+++ volume-mapper/blocks/Cinder-Kinect/src/CinderFreenect.cpp
@@ -208,10 +208,7 @@ void Kinect::depthImageCB( freenect_device *dev, void *d, uint32_t timestamp )
 
 		kinectObj->mDepthBuffers.derefActiveBuffer();					// finished with current active buffer
 		uint16_t *destPixels = kinectObj->mDepthBuffers.getNewBuffer(); // request a new buffer
-		for( size_t p = 0; p < 640 * 480; ++p ) {						// out = 1.0 - ( in / 2048 ) ^ 2
-			uint32_t v = depth[p];
-			destPixels[p] = 65535 - ( v * v ) >> 4;						// 1 / ( 2^10 * 2^10 ) * 2^16 = 2^-4
-		}
+        memcpy(destPixels, depth, 640 * 480 * sizeof(uint16_t));
 		kinectObj->mDepthBuffers.setActiveBuffer( destPixels );			// set this new buffer to be the current active buffer
 		kinectObj->mNewDepthFrame = true;								// flag that there's a new depth frame
 	}
